f486e0ae8ed74e22bcbd52b30cf8bfd041d33211
Niporka <pavelniporka@gmail.com>
2021-10-11 16:52:55 +0300

    demo

---
+++ src/main/java/com/example/demo/model/Category.java
@@ -0,0 +1,36 @@
+package com.example.demo.model;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "category_id")
+
+    private int id;
+
+    private String name;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
---
+++ src/main/java/com/example/demo/service/CategoryService.java
@@ -0,0 +1,26 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Category;
+import com.example.demo.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    public List<Category> getAllCategory() {
+        return categoryRepository.findAll();
+    }
+
+    public void addCategory(Category category) {
+        categoryRepository.save(category);
+
+
+    }
+
+
+}
