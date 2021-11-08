5250e99298a7a8f649ef5d01c7111be05db5b83d
Niporka <pavelniporka@gmail.com>
2021-11-08 16:37:11 +0200

    Demo

--- src/main/java/com/example/demo/configuration/GoogleOAuth2SuccessHandler.java
+++ src/main/java/com/example/demo/configuration/GoogleOAuth2SuccessHandler.java
@@ -54,9 +54,9 @@ public class GoogleOAuth2SuccessHandler implements AuthenticationSuccessHandler 
 
 
 
-//    @Override
-//    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
-//
-//    }
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+
+    }
 
 }
